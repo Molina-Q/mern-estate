5e89decc1e68e21fdeb035e1b216d25210b668ac
Molina-Q <quentinmolina1@gmail.com>
2024-04-09 10:35:20 +0200

    added middleware for error handling

--- api/index.js
+++ api/index.js
@@ -24,3 +24,13 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    succes: false,
+    statusCode, // no need to write property because equal to itself (statusCode: statusCode, message: message) in es6 only
+    message,
+  })
+})
