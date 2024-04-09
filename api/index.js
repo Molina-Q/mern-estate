6964bd4de7c0de70660119c6edbd113e1c5805ba
Molina-Q <quentinmolina1@gmail.com>
2024-04-09 14:36:04 +0200

    fixed typo on 'success'

--- api/index.js
+++ api/index.js
@@ -29,8 +29,8 @@ app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
   return res.status(statusCode).json({
-    succes: false,
+    success: false,
     statusCode, // no need to write property because equal to itself (statusCode: statusCode, message: message) in es6 only
     message,
-  })
-})
+  });
+});
