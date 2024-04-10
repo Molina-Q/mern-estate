72c7685971359689ffeb7e7060dc32476acfefb3
Molina-Q <quentinmolina1@gmail.com>
2024-04-10 16:54:46 +0200

    minor changes

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,7 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
-import userReducer from "./user/userSlice";
-import { persistReducer, persistStore } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({ user: userReducer });
 
@@ -10,7 +9,7 @@ const persistConfig = {
   key: 'root',
   storage,
   version: 1,
-}
+};
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
@@ -22,4 +21,4 @@ export const store = configureStore({
     }),
 });
 
-export const persistor = persistStore(store)
+export const persistor = persistStore(store);
