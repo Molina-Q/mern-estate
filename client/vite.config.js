93eaf6200184bcf62af204840ca803cd7aa51c5b
Molina-Q <quentinmolina1@gmail.com>
2024-04-12 22:39:53 +0200

    added comment

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
+      '/api': { // make it so every url start with "/api" will use the target
         target: 'http://localhost:3000',
         secure: false,
       },
